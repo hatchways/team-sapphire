aab88cf67a5461de490b525e77b46a14c4ccc6ab
Derek <derekjou0@gmail.com>
2019-11-20 13:17:30 -0800

    User model and secret added
    Added user model and secret key for jwt signing

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const connect = process.env.MONGO_URL;
+const Schema = mongoose.Schema;
+
+mongoose.connect(connect, { useNewUrlParser: true });
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  companies: {
+    type: Array,
+    default: []
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
