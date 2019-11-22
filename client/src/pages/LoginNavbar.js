83f28d268dc94d8d1fb80248ecacb63212840a64
ahana <ahanaghosh94@gmail.com>
2019-11-22 02:48:15 -0500

    add navbar for login and add form to login component

---
+++ client/src/pages/LoginNavbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import {
+  AppBar,
+  Toolbar,
+  InputBase,
+  Paper,
+  Typography,
+  IconButton
+} from "@material-ui/core";
+import SettingsIcon from "@material-ui/icons/Settings";
+import SearchIcon from "@material-ui/icons/Search";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    width: "160px"
+  },
+  titlePart: {
+    color: "#30336b"
+  },
+  search: {
+    width: "50%",
+    height: "90%",
+    borderRadius: "50px",
+    margin: "auto"
+  },
+  input: {
+    width: "calc(100% - 58px)",
+    marginLeft: "10px"
+  },
+  settings: {
+    float: "right"
+  }
+}));
+
+const LoginNavbar = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" noWrap className={classes.title}>
+            mentions<span className={classes.titlePart}>crawler</span>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default LoginNavbar;
