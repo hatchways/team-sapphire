629fd8024eac4cf7ea4b38bef5af32328d86054c
Nicky Cen <nickycen@hotmail.com>
2019-11-22 16:24:15 -0500

    changed dashboard to functional component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,82 +1,69 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Grid } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 import Mentions from "./Mentions";
 import Platforms from "./Platforms";
 import Navbar from "./Navbar";
 
-const dashboardStyle = theme => ({
+const useStyles = makeStyles(theme => ({
   rightGridContainer: {
     borderLeft: '1px solid black',
     height: '100%'
   }
-});
+}));
 
-class Dashboard extends Component {
-  state = {
-    platforms: [
-      {name: "Reddit", inUse: true},
-      {name: "Twitter", inUse: true},
-      {name: "Facebook", inUse: true},
-      {name: "Amazon", inUse: true},
-      {name: "Forbes", inUse: true},
-      {name: "Shopify", inUse: true},
-      {name: "Business Insider", inUse: true}
-    ],
-    mentions: [
-      {title: 'thing happened', platform: 'Reddit', desc: 'qwerty'},
-      {title: 'consume', platform: 'Forbes', desc: '12345'},
-      {title: 'buy', platform: 'Shopify', desc: 'abc123'},
-      {title: 'do', platform: 'Business Insider', desc: 'aedsfadwsf'}
-    ],
-    sort: 0
-  };
+function Dashboard() {
+  const [platforms, setPlatforms] = useState([
+    {name: "Reddit", inUse: true},
+    {name: "Twitter", inUse: true},
+    {name: "Facebook", inUse: true},
+    {name: "Amazon", inUse: true},
+    {name: "Forbes", inUse: true},
+    {name: "Shopify", inUse: true},
+    {name: "Business Insider", inUse: true}
+  ]);
+  const [mentions, setMentions] = useState([
+    {title: 'thing happened', platform: 'Reddit', desc: 'qwerty'},
+    {title: 'consume', platform: 'Forbes', desc: '12345'},
+    {title: 'buy', platform: 'Shopify', desc: 'abc123'},
+    {title: 'do', platform: 'Business Insider', desc: 'aedsfadwsf'}
+  ]);
+  const [sort, setSort] = useState(0);
 
-  handlePlatformChange = (key) => {
-    this.setState(prevState => {
-      const platforms = prevState.platforms.map((platform, i) => {
-        if (i === key) {
-          platform.inUse = !platform.inUse;
-        }
-        return platform;
-      });
-      return { platforms }
-    })
+  const handlePlatformChange = (key) => {
+    let updatedPlatforms = [...platforms];
+    updatedPlatforms[key] = !updatedPlatforms[key];
+    setPlatforms(updatedPlatforms);
   }
 
-  handleSortChange = (event, sort) => {
-    console.log(sort)
-    this.setState(prevState => ({
-      sort
-    }));
+  const handleSortChange = (event, sort) => {
+    setSort(sort);
   }
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.dashboardContainer}>
-        <Navbar
-        />
-        <Grid container spacing={0}>
-          <Grid item xs={4}>
-            <Platforms
-              platforms={this.state.platforms}
-              handleChange={this.handlePlatformChange}
-            />
-          </Grid>
-          <Grid item xs={8} className={classes.rightGridContainer}>
-            <Mentions
-              mentions={this.state.mentions}
-              sort={this.state.sort}
-              handleChange={this.handleSortChange}
-            />
-          </Grid>
+  const classes = useStyles();
+  return (
+    <div className={classes.dashboardContainer}>
+      <Navbar
+      />
+      <Grid container spacing={0}>
+        <Grid item xs={4}>
+          <Platforms
+            platforms={platforms}
+            handleChange={handlePlatformChange}
+          />
         </Grid>
-      </div>
-    );
-  }
+        <Grid item xs={8} className={classes.rightGridContainer}>
+          <Mentions
+            mentions={mentions}
+            sort={sort}
+            handleChange={handleSortChange}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default withStyles(dashboardStyle)(Dashboard);
+export default Dashboard;
