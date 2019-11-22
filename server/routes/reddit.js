44119c51600da6a40873cd5093f88c96ed4c6a51
Nicky Cen <nickycen@hotmail.com>
2019-11-22 15:25:15 -0500

    created snoowrap tokens

--- server/routes/reddit.js
+++ server/routes/reddit.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const snoowrap = require('snoowrap');
 
 const r = new snoowrap({
-  userAgent: '',
-  clientId: '',
-  clientSecret: '',
-  refreshToken: ''
+  userAgent: 'mentionscrawler',
+  clientId: 'zib4JtksPnuuTQ',
+  clientSecret: 'CgQwwVAhymykGF4W5-LtQgBgeSw',
+  refreshToken: '27776375-KsPMTA18nr9euoXz9ZuJwu6FIuU',
+  accessToken: '27776375-0sx19eJqWpqnbQ_L2Ac-lwqp150'
 });
+
+const search = r.getSubreddit('all').search({query: 'company', sort: 'new'});
+console.log(search);
 
 router.get("/search", (req, res, next) => {
   const search = r.getSubreddit('all').search({query: 'company', sort: 'new'});
