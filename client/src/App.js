5fd87287ec76b3f48306c528869d7e08afddc483
ahana <ahanaghosh94@gmail.com>
2019-11-22 01:43:34 -0500

    add login and register component routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 import "./App.css";
 
@@ -14,9 +16,10 @@ function App() {
       <BrowserRouter>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
       </BrowserRouter>
     </MuiThemeProvider>
   );
 }
-
 export default App;
---
+++ client/src/pages/Register.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Register = () => {
+  return <div>REgister</div>;
+};
+export default Register;
