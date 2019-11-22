da253a02c3787bc849d0682cb7807c771fe8619e
ncen5293 <nickycen@hotmail.com>
2019-11-22 16:39:25 -0500

    added response to router

--- server/routes/reddit.js
+++ server/routes/reddit.js
@@ -24,14 +24,23 @@ const r = new snoowrap({
 
 router.get("/search/new/:company", (req, res, next) => {
   r.search({query: req.params.company, sort: 'new'})
-    .then(res => {
-      res.forEach((submission, i) => {
+    .then(posts => {
+      let submissions = [];
+      posts.forEach((submission, i) => {
         console.log(submission.title);
-        console.log(submission.subreddit_name_prefixed)
+        console.log(submission.subreddit_name_prefixed);
         console.log(submission.permalink);
         console.log(submission.thumbnail);
         console.log(submission.selftext);
+        submissions.push({
+          title: submission.title,
+          platform: submission.subreddit_name_prefixed,
+          link: submission.permalink,
+          image: submission.thumbnail,
+          desc: submission.selftext
+        });
       });
+      res.send({ success: true, submissions });
     });
 });
 
