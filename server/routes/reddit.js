4ac7ce761621b2688e28f360fe9255fbeb60f724
ncen5293 <nickycen@hotmail.com>
2019-11-24 17:05:51 -0500

    deleted commented out code

--- server/routes/reddit.js
+++ server/routes/reddit.js
@@ -9,19 +9,6 @@ const r = new snoowrap({
   refreshToken: process.env.REDDIT_REFRESH_TOKEN
 });
 
-//example to check if reddit mentions work
-// r.search({query: 'microsoft', sort: 'top'})
-//   .then(res => {
-//     console.log(res[0]);
-//     res.forEach((submission, i) => {
-//       console.log(submission.title);
-//       console.log(submission.subreddit_name_prefixed)
-//       console.log(submission.permalink);
-//       console.log(submission.thumbnail);
-//       console.log(submission.selftext);
-//     });
-//   });
-
 router.get("/search/new/:company", (req, res, next) => {
   r.search({query: req.params.company, sort: 'new'})
     .then(posts => {
