286b6cd6a801def23ac8611b08dabc42079812ab
ahana <ahanaghosh94@gmail.com>
2019-11-26 09:51:58 -0500

    user adds company names and cant save if there is no company name

--- client/src/pages/SettingsBody.js
+++ client/src/pages/SettingsBody.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -15,29 +15,56 @@ import color from "@material-ui/core/colors/grey";
 const useStyles = makeStyles(theme => ({
   bodyContainer: {
     backgroundColor: "#fafbff",
-    height: "100vh"
+    height: "100vh",
+    padding: "5vh"
   },
   companyNameInput: {
-    [`& fieldset`]: {
-      borderRadius: "25px",
-      backgroundColor: "white"
-    }
+    borderRadius: "25px"
+    // backgroundColor: "white"
+  },
+  companyNamesContainer: {
+    display: "flex",
+    flexDirection: "column"
   },
   buttonAdornment: {
     backgroundColor: "#6583f2 ",
     borderRadius: "25px",
     color: "white"
+  },
+  listOfCompanies: {
+    borderRadius: "25px"
+  },
+  error: {
+    color: "red",
+    display: "flex",
+    justifyContent: "center"
   }
 }));
 const SettingsBody = () => {
   const classes = useStyles();
   const history = useHistory();
+  const [companyNameSaveError, setCompanyNameSaveError] = useState("");
+  const [companyNames, setCompanyNames] = useState([]);
+  const [companyNameInput, setCompanyNameInput] = useState("");
 
   const onClickHandler = event => {
     event.preventDefault();
-    console.log(event.target);
+    if (companyNames.length > 0) {
+      history.push("/dashboard");
+    } else {
+      setCompanyNameSaveError("Add at least one company name");
+    }
+  };
+  const onSubmitHandler = event => {
+    event.preventDefault();
+    event.persist();
 
-    history.push("/dashboard");
+    setCompanyNames([...companyNames, event.target.fruitName.value]);
+    setCompanyNameInput("");
+  };
+
+  const onAddHandler = () => {
+    setCompanyNameSaveError("");
   };
 
   return (
@@ -46,19 +73,51 @@ const SettingsBody = () => {
         {" "}
         <b>Your company</b>
       </div>
-      <TextField
-        variant="outlined"
-        margin="normal"
-        required
-        name="company-name"
-        label=""
-        type="text"
-        id="company-name"
-        className={classes.companyNameInput}
-        InputProps={{
-          endAdornment: <Button className={classes.buttonAdornment}>Add</Button>
-        }}
-      />
+      <div>
+        <form onSubmit={onSubmitHandler}>
+          <input
+            type="text"
+            placeholder="Add Company name"
+            title="Add a tag"
+            value={companyNameInput}
+            id="fruitName"
+            onChange={event => setCompanyNameInput(event.target.value)}
+            className={classes.companyNameInput}
+          />
+
+          <button onClick={onAddHandler} className={classes.buttonAdornment}>
+            Add
+          </button>
+        </form>
+        {/* <TextField
+          variant="outlined"
+          margin="normal"
+          // required
+          name="company-name"
+          value={companyNameInput}
+          label=""
+          type="text"
+          id="company-name"
+          className={classes.companyNameInput}
+          onChange={event => setCompanyNameInput(event.target.value)}
+          InputProps={{
+            endAdornment: (
+              <Button onAdd={onAddHandler} className={classes.buttonAdornment}>
+                Add
+              </Button>
+            )
+          }}
+        /> */}
+        <div className={classes.companyNamesContainer}>
+          {companyNames.length > 0 &&
+            companyNames.map((companyName, index) => (
+              <span className={classes.listOfCompanies} key={index}>
+                {" "}
+                {companyName}
+              </span>
+            ))}
+        </div>
+      </div>
       <div>
         {" "}
         <b>Weekly Report </b>
@@ -73,6 +132,7 @@ const SettingsBody = () => {
         id="company-email"
         className={classes.companyNameInput}
       />
+      <p className={classes.error}>{companyNameSaveError}</p>
       <Button onClick={onClickHandler} className={classes.buttonAdornment}>
         Save
       </Button>
