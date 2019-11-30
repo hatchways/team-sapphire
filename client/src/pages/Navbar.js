5b30155c3255a25218a58c77140b97e12cc7886d
Nicky Cen <nickycen@hotmail.com>
2019-11-30 14:54:22 -0500

    reverted navbar css changes

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -17,13 +17,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   title: {
-    width: "160px",
-    overflow: "visible"
-  },
-  loginTitle: {
-    width: "160px",
-    overflow: "visible",
-    flexGrow: 1
+    width: "160px"
   },
   titlePart: {
     color: "#30336b"
@@ -40,7 +34,7 @@ const useStyles = makeStyles(theme => ({
     color: "white"
   },
   search: {
-    width: "calc(100vh - 160px)",
+    width: "100vh",
     height: "90%",
     borderRadius: "50px",
     margin: "auto"
@@ -48,6 +42,9 @@ const useStyles = makeStyles(theme => ({
   input: {
     width: "calc(100% - 58px)",
     marginLeft: "10px"
+  },
+  settings: {
+    float: "right"
   }
 }));
 
@@ -62,45 +59,92 @@ const Navbar = ({
     <div>
       <AppBar position="static">
         <Toolbar>
-          {showSearch && (
-            <>
+          <Grid container justify="space-between">
+            <Grid item>
               <Typography variant="h6" noWrap className={classes.title}>
                 mentions<span className={classes.titlePart}>crawler.</span>
               </Typography>
-              <Paper component="form" className={classes.search}>
-                <InputBase
-                  placeholder="Search Company Name..."
-                  className={classes.input}
-                />
-                <IconButton type="submit" aria-label="search">
-                  <SearchIcon />
-                </IconButton>
-              </Paper>
-              <IconButton>
-                <SettingsIcon />
-              </IconButton>
-            </>
-          )}
-          {!showSearch && (
-            <>
-              <Typography variant="h6" noWrap className={classes.loginTitle}>
-                mentions<span className={classes.titlePart}>crawler.</span>
-              </Typography>
-              <Typography variant="caption" className={classes.linkTitle}>
-                {" "}
-                {showRegister
-                  ? "Already have an account?"
-                  : "Don't have an account"}
-              </Typography>
-              <Button
-                variant="outlined"
-                href={showRegister ? "/login" : "/register"}
-                className={classes.loginRegisterButton}
-              >
-                {showRegister ? "Login" : "Register"}
-              </Button>
-            </>
-          )}
+            </Grid>
+
+            {showSearch && loggedIn && (
+              <>
+                <Grid>
+                  <Paper component="form" className={classes.search}>
+                    <InputBase
+                      placeholder="Search Company Name..."
+                      className={classes.input}
+                    />
+                    <IconButton type="submit" aria-label="search">
+                      <SearchIcon />
+                    </IconButton>
+                  </Paper>
+                </Grid>
+                <Grid>
+                  <IconButton className={classes.settings}>
+                    <SettingsIcon />
+                  </IconButton>
+                </Grid>
+              </>
+            )}
+            {showSearch && !loggedIn && loginToggle && (
+              <>
+                <Grid item>
+                  <Paper component="form" className={classes.search}>
+                    <InputBase
+                      placeholder="Search Company Name..."
+                      className={classes.input}
+                    />
+                    <IconButton type="submit" aria-label="search">
+                      <SearchIcon />
+                    </IconButton>
+                  </Paper>
+                </Grid>
+                <Grid>
+                  <IconButton className={classes.settings}>
+                    <SettingsIcon />
+                  </IconButton>
+                </Grid>
+              </>
+            )}
+            {showSearch && !loggedIn && !loginToggle && (
+              <>
+                <Grid item>
+                  <Paper component="form" className={classes.search}>
+                    <InputBase
+                      placeholder="Search Company Name..."
+                      className={classes.input}
+                    />
+                    <IconButton type="submit" aria-label="search">
+                      <SearchIcon />
+                    </IconButton>
+                  </Paper>
+                </Grid>
+                <Grid item>
+                  <IconButton className={classes.settings}>
+                    <SettingsIcon />
+                  </IconButton>
+                </Grid>
+              </>
+            )}
+
+            {!showSearch && (
+              <Grid item>
+                <Typography variant="caption" className={classes.linkTitle}>
+                  {" "}
+                  {showRegister
+                    ? "Already have an account?"
+                    : "Don't have an account"}
+                </Typography>
+                <Button
+                  variant="outlined"
+                  href={showRegister ? "/login" : "/register"}
+                  className={classes.loginRegisterButton}
+                >
+                  {showRegister ? "Login" : "Register"}
+                </Button>
+              </Grid>
+            )}
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
