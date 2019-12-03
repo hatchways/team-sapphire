cf43b4a12e0fc646460d9a14b6a90a2aab54dcf6
ncen5293 <nickycen@hotmail.com>
2019-12-03 16:27:55 -0500

    stored userId in the authentication

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -2,24 +2,21 @@ const { io } = require("../app");
 const jwt = require("jsonwebtoken");
 const cookie = require('cookie');
 
-const getDecodedToken = (socket) => {
-  const cookies = cookie.parse(socket.handshake.headers.cookie);
-  const token = cookies.token;
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  return decodedToken;
-}
-
 io.use((socket, next) => {
-  if (getDecodedToken(socket)) {
+  const cookies = cookie.parse(socket.handshake.headers.cookie);
+  if (cookies.token) {
+    const token = cookies.token;
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    socket.userId = decodedToken.userId;
     next();
+  } else {
+    next("Unable to authenticate");
   }
 })
 .on("connection", (socket) => {
-  console.log("connected");
-  socket.userId = getDecodedToken(socket).userId;
+  console.log(`${socket.userId} connected`);
 
   socket.on("disconnect", () => {
-    const userId = socket.userId;
-    console.log(`${userId} disconnected`);
+    console.log(`${socket.userId} disconnected`);
   });
 })
