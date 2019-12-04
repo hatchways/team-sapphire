b3548f5a126f11095a2907962a7d084a66575272
ahana <ahanaghosh94@gmail.com>
2019-12-04 10:02:08 -0500

    put more details on mention dialog like desc image

--- client/src/pages/MentionDialog.js
+++ client/src/pages/MentionDialog.js
@@ -2,16 +2,44 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Paper,
+  Tabs,
+  Tab,
+  Link as LinkTo
+} from "@material-ui/core";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
 
 import { blue } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600]
+  mentionsContainer: {
+    width: "75%",
+    margin: "auto",
+    marginTop: "10px"
+  },
+  sortToggleContainer: {
+    float: "right",
+    borderRadius: "50px",
+    color: "#30336b"
+  },
+  cardContainer: {
+    marginBottom: "10px",
+    display: "flex",
+    height: "18vh",
+    maxHeight: "18vh"
+  },
+  cardImage: {
+    width: "30%"
+  },
+  header: {
+    marginBottom: "20px",
+    height: window.innerWidth > 1024 ? "50px" : "100px"
   }
 });
 
@@ -30,7 +58,31 @@ function SimpleDialog(props) {
       aria-labelledby="simple-dialog-title"
       open={open}
     >
-      <DialogTitle id="simple-dialog-title">{mention.title}</DialogTitle>
+      <DialogTitle id="simple-dialog-title">
+        <Typography variant="h5" component="h2">
+          <LinkTo href={mention.link} rel="noopener">
+            {mention.title}
+          </LinkTo>
+        </Typography>
+      </DialogTitle>
+
+      <CardMedia
+        component="img"
+        alt="image"
+        image={mention.image}
+        title="Image"
+        className={classes.cardImage}
+      />
+      <div>
+        <CardContent>
+          <Typography>
+            <LinkTo href={mention.link} rel="noopener">
+              {mention.platform}
+            </LinkTo>
+          </Typography>
+          <Typography>{mention.desc}</Typography>
+        </CardContent>
+      </div>
     </Dialog>
   );
 }
@@ -44,7 +96,6 @@ const MentionDialog = ({ match, mentions, open, setOpen }) => {
     setOpen(false);
   };
 
-  console.log("inside mentiondialog with open: ", open);
   const mention = mentions.find((mention, id) => {
     return id === Number(match.params.mentionId);
   });
