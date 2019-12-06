c2f02696cbb8b5164fa362e52046b6d9f27d6654
ahana <ahanaghosh94@gmail.com>
2019-12-06 10:33:08 -0500

    add skeletal structure for email queue

--- server/services/emails/email.js
+++ server/services/emails/email.js
@@ -1,4 +1,5 @@
 import Queue from "bull";
+import Mention from "../../models/Mention";
 const emailQueue = new Queue("emailQueue", "redis://127.0.0.1:6379");
 
 const delayedEmailOptions = {};
@@ -10,3 +11,37 @@ const repeat = {
 const weeklyEmailOptions = {
   repeat
 };
+emailQueue.process(async job => {
+  const result = {};
+
+  return result;
+});
+//We use this promisified timeout to simulate async work
+const mockAsync = number => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!number) reject(new Error("no first name"));
+      resolve(number * 10);
+    }, 5000);
+  });
+};
+//here you declared the actual job, what you want to do asynchronously
+timeoutQueue.process(async (job, done) => {
+  const result = await mockAsync(job.data.number);
+  done(null, result);
+});
+//here you're adding a job to the queue and making it repeat at an interbal
+timeoutQueue.add(
+  { number: 4 },
+  //Optional: you can give the job an id to be able to locate in the queue
+  { jobId: "someId", repeat: { every: 1000 } }
+); //repeat every n miliseconds
+//Inside this listener, you define what you'll do with the result.
+timeoutQueue.on("completed", async (job, result) => {
+  console.log(
+    "the job data was: ",
+    job.data.number,
+    "and this was the result ",
+    result
+  );
+});
