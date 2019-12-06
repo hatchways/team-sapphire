1d82c55f0ac340c70540fa964f8657b31ce0ecb7
Nicky Cen <nickycen@hotmail.com>
2019-12-05 20:14:06 -0500

    created helper function to get company summary and removed unneeded div

--- client/src/pages/Mention.js
+++ client/src/pages/Mention.js
@@ -50,78 +50,81 @@ const Mention = ({ mention, index, setOpen }) => {
     setOpen(true);
   };
 
-  let title = mention.title;
-  let desc = mention.desc;
-  let titleIndex = title.search(mention.company) > -1 ? title.search(mention.company) : title.search(mention.company.toLowerCase());
-  let titleEnd = titleIndex + mention.company.length;
-  let descIndex = desc.search(mention.company) > -1 ? desc.search(mention.company) : desc.search(mention.company.toLowerCase());
-  let descEnd = descIndex + mention.company.length;
-  if (desc.length > 200 && descEnd >= 200) {
-    desc = "..." + desc.substring(descIndex - 100, descIndex + 100) + "...";
-    descIndex = descEnd - (descEnd - 100) + 3;
-    descEnd = descIndex + mention.company.length;
-  } else if (desc.length > 200 && descEnd < 200) {
-    desc = desc.substring(0, 200) + "...";
-  }
-  if (descIndex > -1) {
-    desc = <span>
-            {desc.substring(0, descIndex)}
-            <span className={classes.companyName}>
-              {desc.substring(descIndex, descEnd)}
-            </span>
-            {desc.substring(descEnd)}
-          </span>
-  }
-  if (title.length > 40 && titleEnd >= 40) {
-    title = "..." + title.substring(titleIndex - 20, titleIndex + 20) + "...";
-    titleIndex = titleEnd - (titleEnd - 20) + 3;
-    titleEnd = titleIndex + mention.company.length;
-  } else if (title.length > 40 && titleEnd < 40) {
-    title = title.substring(0, 40) + "...";
-  }
-  if (titleIndex > -1) {
-    title = <span>
-              {title.substring(0, titleIndex)}
+  const getCompanySummary = (title, desc, company) => {
+    let titleIndex = title.search(company) > -1 ? title.search(company) : title.search(company.toLowerCase());
+    let titleEnd = titleIndex + company.length;
+    let descIndex = desc.search(company) > -1 ? desc.search(company) : desc.search(company.toLowerCase());
+    let descEnd = descIndex + company.length;
+    if (desc.length > 200 && descEnd >= 200) {
+      desc = "..." + desc.substring(descIndex - 100, descIndex + 100) + "...";
+      descIndex = descEnd - (descEnd - 100) + 3;
+      descEnd = descIndex + company.length;
+    } else if (desc.length > 200 && descEnd < 200) {
+      desc = desc.substring(0, 200) + "...";
+    }
+    if (descIndex > -1) {
+      desc = <span>
+              {desc.substring(0, descIndex)}
               <span className={classes.companyName}>
-                {title.substring(titleIndex, titleEnd)}
+                {desc.substring(descIndex, descEnd)}
               </span>
-              {title.substring(titleEnd)}
+              {desc.substring(descEnd)}
             </span>
+    }
+    if (title.length > 40 && titleEnd >= 40) {
+      title = "..." + title.substring(titleIndex - 20, titleIndex + 20) + "...";
+      titleIndex = titleEnd - (titleEnd - 20) + 3;
+      titleEnd = titleIndex + company.length;
+    } else if (title.length > 40 && titleEnd < 40) {
+      title = title.substring(0, 40) + "...";
+    }
+    if (titleIndex > -1) {
+      title = <span>
+                {title.substring(0, titleIndex)}
+                <span className={classes.companyName}>
+                  {title.substring(titleIndex, titleEnd)}
+                </span>
+                {title.substring(titleEnd)}
+              </span>
+    }
+    return {title, desc};
   }
 
+  const summary = getCompanySummary(mention.title, mention.desc, mention.company);
+  const title = summary.title;
+  const desc = summary.desc;
+
   return (
-    <div>
-      <Link to={`/dashboard/mentions/${index}`} className={classes.link}>
-        <Card
-          className={classes.cardContainer}
-          key={index}
-          onClick={handleClickOpen}
-        >
-          <Grid container>
-            <Grid item xs={3} className={classes.imageContainer}>
-              <CardMedia
-                component="img"
-                alt="image"
-                image={mention.image}
-                title="Image"
-                className={classes.cardImage}
-              />
-            </Grid>
-            <Grid item xs={9} className={classes.contentContainer}>
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                  <LinkTo href={mention.link} rel="noopener" className={classes.link}>
-                    {mention.title}
-                  </LinkTo>
-                </Typography>
-                <Typography>{mention.platform}</Typography>
-                <Typography>{mention.desc}</Typography>
-              </CardContent>
-            </Grid>
+    <Link to={`/dashboard/mentions/${index}`} className={classes.link}>
+      <Card
+        className={classes.cardContainer}
+        key={index}
+        onClick={handleClickOpen}
+      >
+        <Grid container>
+          <Grid item xs={3} className={classes.imageContainer}>
+            <CardMedia
+              component="img"
+              alt="image"
+              image={mention.image}
+              title="Image"
+              className={classes.cardImage}
+            />
           </Grid>
-        </Card>
-      </Link>
-    </div>
+          <Grid item xs={9} className={classes.contentContainer}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                <LinkTo href={mention.link} rel="noopener" className={classes.link}>
+                  {title}
+                </LinkTo>
+              </Typography>
+              <Typography>{mention.platform}</Typography>
+              <Typography>{desc}</Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
+      </Card>
+    </Link>
   );
 };
 
